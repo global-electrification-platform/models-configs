ea9ad8480e78dd46bd69523aa4588d1a01ae7e1f
Eric Soroos <eric@derilinx.com>
2021-08-25 14:44:41 +0200

    source data scrips

--- bin/add_source.py
+++ bin/add_source.py
@@ -6,13 +6,16 @@ with open('gep-data.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         iso2 = row['ISO2']
-        url = "https://%s" % (row['Final Link'])
-        model = "%s-1" % (iso2)
+        if 'http' in row['Final Link']:
+            url = row['Final Link']
+        else:
+            url = "https://%s" % (row['Final Link'])
+        model = "%s-2" % (iso2)
         print (model, url)
         # warning, funky spacing required
         proc = subprocess.run("cat >> data/%s.yml" % model, shell=True,
                               encoding='utf8',
                               input="""sourceData:
   scenarios: '%s'
+  clusters: null
 """ % url)
-        
---
+++ bin/remove-sourcedata.sh
@@ -0,0 +1,3 @@
+sed -i /sourceData:/d *-2.yml
+sed -i '/  clusters:/d' *-2.yml
+sed -i '/  scenarios:/d' *-2.yml
